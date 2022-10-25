8f998cf0daca2545e716d1cf51325641961416e8
mairjul <mairjul@students.zhaw.ch>
2022-10-25 18:08:09 +0200

    closes issues #3

--- src/main/java/ch/zhaw/checkout/checkout/TwoForOneVoucher.java
+++ src/main/java/ch/zhaw/checkout/checkout/TwoForOneVoucher.java
@@ -2,17 +2,22 @@ package ch.zhaw.checkout.checkout;
 
 import java.util.List;
 
-public class TwoForOneVoucher implements Voucher{
-private Product product;
+public class TwoForOneVoucher implements Voucher {
 
-public TwoForOneVoucher(Product product){
+    private Product product;
 
-    
-}
+    public TwoForOneVoucher(Product product) {
 
-@Override
+    }
+
+    @Override
     public double getDiscount(List<Product> products) {
-        // TODO Auto-generated method stub
-        return 0;
+        int count; /*
+        if (count = products.stream().filter(x -> x.getId.equals(product)).count() >=2){
+                return 0.0;
+                 
+            }*/
+        return 0.0;
+        
     }
 }
