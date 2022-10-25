88cf6a5434e91b2f917e4037b23a5364870295c5
mairjul <mairjul@students.zhaw.ch>
2022-10-25 12:54:44 +0200

    Cleaning and building of percentagetest class

--- src/test/java/ch/zhaw/checkout/checkout/FiveBucksVoucherTest.java
+++ src/test/java/ch/zhaw/checkout/checkout/FiveBucksVoucherTest.java
@@ -1,18 +1,24 @@
 package ch.zhaw.checkout.checkout;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 
 public class FiveBucksVoucherTest {
-    
+
     @Test
     public void testEmpty() {
-        assertTrue(FiveBucksVoucher.isEven(0));
+        ArrayList<Product> products = new ArrayList<Product>();
+        FiveBucksVoucher fiveBucksVoucher = new FiveBucksVoucher();
+        assertEquals(fiveBucksVoucher.getDiscount(products), 0.0);
     }
 
     @Test
-    public void testTen(){
-
+    public void testTen() {
+        ArrayList<Product> products = new ArrayList<Product>();
+        FiveBucksVoucher fiveBucksVoucher = new FiveBucksVoucher();
+        Product pro1 = new Product("88", "Cola ", "Getränk", 10.00);
+        products.add(pro1);
+        assertEquals(fiveBucksVoucher.getDiscount(products), 5.0);
     }
-   
 }
