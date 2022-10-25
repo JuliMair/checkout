d58b690546e4c530a3f357ba74daf56f9bf6e828
mairjul <mairjul@students.zhaw.ch>
2022-10-25 13:15:45 +0200

    closes issue #2

--- src/main/java/ch/zhaw/checkout/checkout/PercentageVoucher.java
+++ src/main/java/ch/zhaw/checkout/checkout/PercentageVoucher.java
@@ -2,17 +2,23 @@ package ch.zhaw.checkout.checkout;
 
 import java.util.List;
 
+public class PercentageVoucher implements Voucher {
 
-public class PercentageVoucher implements Voucher{
     private int discount;
 
-    public PercentageVoucher(int discount){
+    public PercentageVoucher(int discount) {
 
     }
 
     @Override
     public double getDiscount(List<Product> products) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (discount != 0) {
+            int CHFdiscount = (int) (products.stream().mapToDouble(x-> x.getPrice()).sum()*(discount/100));
+            return discount = CHFdiscount;
+            
+        } else {
+            return 0.0;
+        }
+        
     }
 }
