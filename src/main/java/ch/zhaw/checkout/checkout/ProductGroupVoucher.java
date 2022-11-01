567331453bad6253ad8813ea035a1004a8fa6731
mairjul <mairjul@students.zhaw.ch>
2022-11-01 13:43:04 +0100

    closes exercise e

--- src/main/java/ch/zhaw/checkout/checkout/ProductGroupVoucher.java
+++ src/main/java/ch/zhaw/checkout/checkout/ProductGroupVoucher.java
@@ -2,30 +2,32 @@ package ch.zhaw.checkout.checkout;
 
 import java.util.List;
 
-public class ProductGroupVoucher implements Voucher{
+public class ProductGroupVoucher implements Voucher {
     private int discount;
     private String productGroup;
 
     public ProductGroupVoucher(int discount, String productGroup) {
         this.discount = discount;
         this.productGroup = productGroup;
-}
+    }
 
     @Override
+
     public double getDiscount(List<Product> products) {
-        if (productGroup == null || productGroup == " ") {
-            throw new RuntimeException("Error: Productgroup must contain a name");
+
+        long vorhanden = products.stream().filter(x -> x.getProductGroup().equals(productGroup)).count();
+
+        if (productGroup == " " || productGroup == null) {
+            throw new RuntimeException("ProdLeer");
+        } else if (products.stream().mapToDouble(x -> x.getPrice()).sum() < discount) {
+            throw new RuntimeException("Error – discount is higher than the shopping cart");
+        } else if(discount <= 0){
+            throw new RuntimeException("Error- discount can not be less than 1");
+        }else if (vorhanden > 0) {
+            return discount;
         }
-        long count = products.stream().filter(x -> x.getProductGroup().equals(productGroup)).count();
-        if (count > 0) {
-            if (discount >= products.stream().mapToDouble(x -> x.getPrice()).sum()) {
-                return products.stream().mapToDouble(x -> x.getPrice()).sum();
-            } 
-        } else {
-                return (double) discount;
-            }
-        return 0.0;
+
+        return 0;
+
     }
 }
-
-
--- src/test/java/ch/zhaw/checkout/checkout/ProductGroupVoucherTest.java
+++ src/test/java/ch/zhaw/checkout/checkout/ProductGroupVoucherTest.java
@@ -1,7 +1,10 @@
 package ch.zhaw.checkout.checkout;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
@@ -9,23 +12,44 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ProductGroupVoucherTest {
-    
 
     @ParameterizedTest
-    @ValueSource(ints = { 1, 2, 5, 20, 49 })
-    public void testEven3(int value) {
+
+    @ValueSource(ints = { 1, 2, 5, 20, 49, 50 })
+
+    public void testExerciseE(int value) {
         ArrayList<Product> products = new ArrayList<Product>();
-        PercentageVoucher percentageVoucher = new PercentageVoucher(value);
-        Product pro1 = new Product("88", "Cola ", "a", 42.00);
-        Product pro2 = new Product("89", "Fanta ", "a", 77.00);
-        products.add(pro1);
-        products.add(pro2);
+        ProductGroupVoucher productGroupVoucher = new ProductGroupVoucher(value, "a");
+        Product p1 = mock(Product.class);
+        Product p2 = mock(Product.class);
+        when(p1.getPrice()).thenReturn(102.0);
+        when(p2.getPrice()).thenReturn(402.0);
+        when(p1.getProductGroup()).thenReturn("a");
+        when(p2.getProductGroup()).thenReturn("a");
+        products.add(p1);
+        products.add(p2);
+        assertEquals(value, productGroupVoucher.getDiscount(products), 0.01);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {  0, -1, -Integer.MAX_VALUE })
+    public void testExerciseEwithfails(int value) {
+        ArrayList<Product> products = new ArrayList<Product>();
+        ProductGroupVoucher productGroupVoucher = new ProductGroupVoucher(value, "a");
+        Product p1 = mock(Product.class);
+        Product p2 = mock(Product.class);
+        when(p1.getPrice()).thenReturn(77.00);
+        when(p2.getPrice()).thenReturn(42.00);
+        when(p1.getProductGroup()).thenReturn("a");
+        when(p2.getProductGroup()).thenReturn("a");
+        products.add(p1);
+        products.add(p2);
         Exception exception = assertThrows(RuntimeException.class, () -> {
-            percentageVoucher.getDiscount(products);
+            productGroupVoucher.getDiscount(products);
         });
-        String expectedMessage = "Error: Discount value must be greater than";
+        String expectedMessage = "Error- discount can not be less than 1";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
-
     }
+
 }
